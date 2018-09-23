3a347cef7ff0120e19c200ea7754a45a82a9d632
Tyus Durant <tyusdurant@Tyuss-MBP-2.triad.rr.com>
2018-09-23 19:20:23 -0400

    endofday1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code React apps</h1>
+    <div className="hero">
+      <div className="heroGroup">
+      <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <Link to="/page-2/">Watch video</Link>
+      </div>
+    </div>
+
   </Layout>
 )
 
